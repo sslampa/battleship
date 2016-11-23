3257aeba634d2ea4c72b6e46370d84be3079da14
Learn <learn@learn07.attlocal.net>
2016-11-23 13:59:15 -0800

    Added win/loss condition and counter

--- js/controller.js
+++ js/controller.js
@@ -9,14 +9,35 @@ $(document).ready(function() {
   }
 
   //add ships to board and change symbols
-  // var shipArray = addShips([1, 1, 1, 1, 1], board, n);
-  // for (var i = 0; i < shipArray.length; i++) {
-  //   $("#" + shipArray[i]).text("X");
-  // }
+  var shipArray = addShips([1, 1, 1, 1, 1], board, n, numShips);
+  for (var i = 0; i < shipArray.length; i++) {
+    $("#" + shipArray[i]).text("X");
+  }
 
   //Change background color based on hit or miss
   $("td").on("click", function() {
-    $(this).addClass("hit");
+
+    //updates users missile count
+    $("#counter").text("You have " + missileCheck());
+
+    //changes class based on hit or miss
+    if (checkHitOrMiss($(this).attr("id"), shipArray)) {
+      $(this).addClass("hit");
+      if (checkWin(shipArray) === 1) {
+        $("#winOrLose").text("You win!");
+        $("td").off();
+      } else if (checkWin(shipArray) === 0) {
+        $("#winOrLose").text("You lose!");
+      }
+    } else {
+      $(this).addClass("miss");
+      if (checkWin(shipArray) === 0) {
+        $("#winOrLose").text("You lose!");
+        $("td").off();
+      }
+    }
+
     $(this).off();
+
   });
 });
--- js/model.js
+++ js/model.js
@@ -5,15 +5,49 @@ var EMPTY = 0;
 var HIT = 1;
 var SHIP = 200;
 var ships = [1, 1, 1, 1, 1];
-var shipsLength = ships.length;
+var numShips = ships.length;
+var counter = 25;
+
+//check wheather you win or lose and check weather you have run out of missiles
+function checkWin(shipArray) {
+  if (shipArray.length === 0 && counter > 0) {
+    return 1;
+  } else if (counter === 0){
+    return 0;
+  } 
+}
+
+//Check for hit or miss at position
+function checkHitOrMiss(idAtPos, shipArray) {
+  //check if it is included in our ship position array
+  //if it is in ship array, it is a hit => true
+  //else, it is a miss=> false
+  if (shipArray.includes(idAtPos)) {
+    console.log(shipArray.splice(shipArray.indexOf(idAtPos), 1));
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//This updates your missile count
+function missileCheck() {
+  counter--;
+  return counter;
+}
 
 //Creates empty board
 function makeBoard(n) {
-  var newBoard = [] //empty board to be returned
 
+  //empty board to be returned
+  var newBoard = []
+
+  //Iterating through the n number of rows
   for (var iter = 0; iter < n; iter++) {
-    var emptyArray = []; //temporary array that holds values
+    //temporary array that will push empty values to newBoard
+    var emptyArray = [];
 
+    //Iterates through the n number of columns
     for (var i = 0; i < n; i++) {
       emptyArray.push(0);
     }
@@ -25,28 +59,48 @@ function makeBoard(n) {
 }
 
 //Generates ships at random positions on the board
-// function addShips(ships, board, n, shipsLength) {
-//   var array = randPos(shipsLength, n);
-//   var shipArray = [];
-//   ships.forEach(function(item, iter) {
-//     board[array[iter][0]][array[iter][1]] = SHIP;
-//     shipArray.push(array[iter].join("-"));
-//   });
-//
-//   return shipArray;
-// }
+function addShips(ships, board, n, numShips) {
+  //Gets and sets position for the number of ships
+  var array = randPos(numShips, n);
+  //Holds the updated indexes for ship placement
+  var shipArray = [];
 
-// made a function to generate random positions on the board
-// 0 index is our row and 1 index is our column
-function randPos(shipsLength, n) {
+  //Takes the values of each ship and gives the value of SHIP(200) at
+  //that board locattion.
+  ships.forEach(function(item, iter) {
+    //Changes the board at board[row][col] for SHIP(200)
+    board[array[iter][0]][array[iter][1]] = SHIP;
+    //Changes the indexes to better match the IDs for each td
+    shipArray.push(array[iter].join("-"));
+  });
+
+  return shipArray;
+}
+
+//Function that generates random positions on the board
+function randPos(numShips, n) {
+
+  //Holds values for the random positions of ships
   var positionArray = [];
+
+  //State will be false if the randomly generated positions are not currently
+  //found in positionArray. True if otherwise.
   var state = false;
 
-  for (var i = 0; i < shipsLength; i++) {
+  //Get position for each ship and make sure they do not overlap
+  for (var i = 0; i < numShips; i++) {
+
+    //Generates potential random position
     var currentPosition =
       [Math.floor(Math.random() * n), Math.floor(Math.random() * n)];
 
+    //Checks the positionArray if it contains the potential random position as
+    //long as the length of the array is greater than 0 to avoid comparing the
+    //first empty value.
     while (true && positionArray.length > 0) {
+
+      //Goes through each individual item in positionArray, changes state at any
+      //point where the currentPosition and current array value are equal.
       for (var x = 0; x < positionArray.length; x++) {
         if (positionArray[x][0] === currentPosition[0] &&
           positionArray[x][1] === currentPosition[1]) {
@@ -55,6 +109,8 @@ function randPos(shipsLength, n) {
           }
       }
 
+      //Checks to see if currentPosition needs to be changed. Breaks if the
+      //currentPosition is not found in the loop.
       if (state) {
         currentPosition = [Math.floor(Math.random() * n),
           Math.floor(Math.random() * n)];
