7189c1e9fdc20cb9c4b5924d3ea2a6988bc0639a
Learn <learn@learn07.attlocal.net>
2016-11-23 11:06:04 -0800

    Added ships

--- js/model.js
+++ js/model.js
@@ -5,11 +5,12 @@ var EMPTY = 0;
 var HIT = 1;
 var SHIP = 200;
 
+//Creates empty board
 function makeBoard(n) {
-  var newBoard = []
+  var newBoard = [] //empty board to be returned
 
   for (var iter = 0; iter < n; iter++) {
-    var emptyArray = [];
+    var emptyArray = []; //temporary array that holds values
 
     for (var i = 0; i < n; i++) {
       emptyArray.push(0);
@@ -19,4 +20,22 @@ function makeBoard(n) {
   }
 
   return newBoard;
+}
+
+//Generates ships at random positions on the board
+function addShips(ships, board, n) {
+  var array = [];
+  board.forEach(function() {
+    var place = randPos(n);
+    board[place[0]][place[1]] = SHIP;
+    array.push(place.join("-"));
+  });
+
+  return array;
+}
+
+// made a function to generate random positions on the board
+// 0 index is our row and 1 index is our column
+function randPos(n) {
+  return [Math.floor(Math.random() * n), Math.floor(Math.random() * n)];
 }
