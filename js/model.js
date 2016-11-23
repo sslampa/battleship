b293b8e1a27a239c19341ce2a4ad37f324596410
Learn <learn@learn07.attlocal.net>
2016-11-23 10:32:25 -0800

    Created board in model.js

--- js/model.js
+++ js/model.js
@@ -1,1 +1,22 @@
-var n = 10; //nxn matrix 
+var n = 10; //nxn matrix
+var board = makeBoard(n);
+var MISS = -1;
+var EMPTY = 0;
+var HIT = 1;
+var SHIP = 200;
+
+function makeBoard(n) {
+  var newBoard = []
+
+  for (var iter = 0; iter < n; iter++) {
+    var emptyArray = [];
+
+    for (var i = 0; i < n; i++) {
+      emptyArray.push(0);
+    }
+
+    newBoard.push(emptyArray);
+  }
+
+  return newBoard;
+}
